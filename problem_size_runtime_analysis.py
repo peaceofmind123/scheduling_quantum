42e3b24ae0dd0f3ac8b3b6f04d52e73119db81cc
ashishthesatan <ashishthesatan@gmail.com>
2022-07-09 11:50:46 +0545

    dataset generation testing done

--- problem_size_runtime_analysis.py
+++ problem_size_runtime_analysis.py
@@ -14,14 +14,25 @@ def generate_dataset(save_path, num_tasksystems_per_iteration, max_num_tasks, ma
     :return:
     """
     dataset = {}
-    for i in range(2, max_num_tasks + 1):
-        dataset[f'{i}'] = {}
-        for j in range(2, max_num_processors + 1):
-            task_system_generator = TaskSystemGenerator(i, j, min_deadline,
+    for num_tasks in range(2, max_num_tasks + 1):
+        dataset[f'{num_tasks}'] = {}
+        for num_processors in range(2, max_num_processors + 1):
+            # number of tasks should be >= number of processors
+            if num_tasks < num_processors:
+                continue
+            task_system_generator = TaskSystemGenerator(num_tasks, num_processors, min_deadline,
                                                         max_deadline, exp_scale)
             task_systems: List[TaskSystem] = task_system_generator.generate_dataset(num_tasksystems_per_iteration,
                                                                                     '', False)  # do not save
-            dataset[f'{i}'][f'{j}'] = task_systems
+            dataset[f'{num_tasks}'][f'{num_processors}'] = task_systems
 
     with open(save_path,'wb') as f:
         pickle.dump(dataset, f)
+
+
+if __name__ == '__main__':
+    save_path = './datasets/dataset_problem_size_runtime_1.pkl'
+    num_task_systems_per_iteration = 10
+    max_num_tasks = 10
+    max_num_processors = 10
+    generate_dataset(save_path,num_task_systems_per_iteration, max_num_tasks,max_num_processors)

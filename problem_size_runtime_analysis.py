303c9895d98b73ff227deb96da57d8941dd84588
ashishthesatan <ashishthesatan@gmail.com>
2022-07-09 12:14:44 +0545

    done with branch and bound runtime and solution data generation code

--- problem_size_runtime_analysis.py
+++ problem_size_runtime_analysis.py
@@ -1,8 +1,9 @@
 from typing import List
-
+from branch_and_bound_solution import BranchBoundSolver
 from TaskSystemGeneration import TaskSystemGenerator
 from task_system import TaskSystem
 import pickle
+import time
 """ Initially, independent functional units will be written
     as the full picture of the analysis is not in my head yet"""
 
@@ -29,6 +30,43 @@ def generate_dataset(save_path, num_tasksystems_per_iteration, max_num_tasks, ma
     with open(save_path,'wb') as f:
         pickle.dump(dataset, f)
 
+def find_branch_bound_solutions_to_dataset(dataset_path, results_save_path):
+    """
+    Find the branch and bound solutions to the entire dataset and save the solutions.
+    Also, the runtimes will be recorded
+    The strategy is to augment the dataset object with solution info and runtime info
+    And save it in the same heirarchical (json-like) format
+    """
+
+    with open(dataset_path, 'rb') as f:
+        dataset = pickle.load(f)
+
+    # iterate through the dataset
+    for num_tasks in dataset: # num_tasks is the first key
+        for num_processors in dataset[num_tasks]:
+            task_systems_set = dataset[num_tasks][num_processors]
+
+            # this notation is needed for overwriting the values of the list
+            for i in range(len(task_systems_set)):
+                # initialize and solve the task system
+                task_system = task_systems_set[i]
+                bbs = BranchBoundSolver(task_system)
+
+                start = time.time_ns()
+                partitioning, optimal_objective = bbs.solve(False) # don't display output
+                end = time.time_ns()
+                elapsed = end - start
+
+                # now, replace the task system with the dict containing relevant info
+                task_system_info = {
+                    'task_system': task_system,
+                    'partitioning': partitioning,
+                    'runtime_ns': elapsed
+                }
+                task_systems_set[i] = task_system_info
+
+    with open(results_save_path, 'wb') as f:
+        pickle.dump(dataset, f)
 
 if __name__ == '__main__':
     save_path = './datasets/dataset_problem_size_runtime_1.pkl'

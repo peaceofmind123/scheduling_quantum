9436de8311d372d5592962e5add6ac32cec6ae1d
ashishthesatan <ashishthesatan@gmail.com>
2022-07-10 10:26:26 +0545

    extracted runtimes for corresponding problem sizes

--- problem_size_runtime_analysis.py
+++ problem_size_runtime_analysis.py
@@ -4,6 +4,7 @@ from TaskSystemGeneration import TaskSystemGenerator
 from task_system import TaskSystem
 import pickle
 import time
+
 """ Initially, independent functional units will be written
     as the full picture of the analysis is not in my head yet"""
 
@@ -27,8 +28,9 @@ def generate_dataset(save_path, num_tasksystems_per_iteration, max_num_tasks, ma
                                                                                     '', False)  # do not save
             dataset[f'{num_tasks}'][f'{num_processors}'] = task_systems
 
-    with open(save_path,'wb') as f:
+    with open(save_path, 'wb') as f:
         pickle.dump(dataset, f)
+
 
 def find_branch_bound_solutions_to_dataset(dataset_path, results_save_path):
     """
@@ -45,7 +47,7 @@ def find_branch_bound_solutions_to_dataset(dataset_path, results_save_path):
     num_infeasible_tasksystems = 0
     total_num_tasksystems = 0
     # iterate through the dataset
-    for num_tasks in dataset: # num_tasks is the first key
+    for num_tasks in dataset:  # num_tasks is the first key
         for num_processors in dataset[num_tasks]:
             task_systems_set = dataset[num_tasks][num_processors]
 
@@ -58,7 +60,7 @@ def find_branch_bound_solutions_to_dataset(dataset_path, results_save_path):
                 bbs = BranchBoundSolver(task_system)
 
                 try:
-                    partitioning, optimal_objective, runtime_ms = bbs.solve(False) # don't display output
+                    partitioning, optimal_objective, runtime_ms = bbs.solve(False)  # don't display output
                 except TypeError as e:
                     # this happens only when the system does not have a feasible solution
                     partitioning = 'infeasible'
@@ -76,6 +78,7 @@ def find_branch_bound_solutions_to_dataset(dataset_path, results_save_path):
     with open(results_save_path, 'wb') as f:
         pickle.dump(dataset, f)
 
+
 def generate_dataset_script():
     save_path = './datasets/dataset_problem_size_runtime_1.pkl'
     num_task_systems_per_iteration = 10
@@ -87,8 +90,50 @@ def generate_dataset_script():
 def generate_solution_dataset_script():
     dataset_path = './datasets/dataset_problem_size_runtime_1.pkl'
     solution_save_path = './datasets/dataset_solutions_problem_size_runtime_1.pkl'
-    find_branch_bound_solutions_to_dataset(dataset_path,solution_save_path)
+    find_branch_bound_solutions_to_dataset(dataset_path, solution_save_path)
 
+
+def get_problem_size_and_runtime_from_solution_dataset():
+    solution_save_path = './datasets/dataset_solutions_problem_size_runtime_1.pkl'
+    with open(solution_save_path, 'rb') as f:
+        dataset = pickle.load(f)
+
+    problem_sizes_to_runtime_dict = {}
+
+    for num_tasks in dataset:  # num_tasks is the first key
+        for num_processors in dataset[num_tasks]:
+            task_systems_set = dataset[num_tasks][num_processors]
+
+            for i in range(len(task_systems_set)):
+                task_system_info = task_systems_set[i]
+                runtime = task_system_info['runtime_ms']
+                # check for feasibility
+                if runtime == 'infeasible':
+                    continue
+
+                task_system = task_system_info['task_system']
+                problem_size = task_system.num_tasks * \
+                               task_system.num_processors
+
+                # if the dictionary key already has an associated list, just append
+                # otherwise create a list and add
+                if problem_size in problem_sizes_to_runtime_dict:
+                    problem_sizes_to_runtime_dict[problem_size].append(runtime)
+                else:
+                    problem_sizes_to_runtime_dict[problem_size] = [runtime]
+    return problem_sizes_to_runtime_dict
+
+def generate_problem_size_vs_runtime_curve(problem_size_and_runtime_dict):
+    pass
+
+def generate_curves(xss, yss):
+    """
+    Generate multiple curves on the same plot
+    :param xss: a list of xs
+    :param yss: a list of corresponding ys
+    :return:
+    """
 
 if __name__ == '__main__':
+    #get_problem_size_and_runtime_from_solution_dataset()
     generate_solution_dataset_script()

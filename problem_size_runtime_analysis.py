1dad5b53ca3cd64480f656dbc12de3894c53a559
ashishthesatan <ashishthesatan@gmail.com>
2022-07-16 09:38:24 +0545

    state before analysis of annealing solution

--- problem_size_runtime_analysis.py
+++ problem_size_runtime_analysis.py
@@ -4,6 +4,7 @@ from TaskSystemGeneration import TaskSystemGenerator
 from task_system import TaskSystem
 import pickle
 import time
+import matplotlib.pyplot as plt
 
 """ Initially, independent functional units will be written
     as the full picture of the analysis is not in my head yet"""
@@ -31,6 +32,11 @@ def generate_dataset(save_path, num_tasksystems_per_iteration, max_num_tasks, ma
     with open(save_path, 'wb') as f:
         pickle.dump(dataset, f)
 
+def find_solution_to_dataset_initialization(dataset_path):
+    """
+    The initialization steps common to all functions to find solution from a dataset
+    :return:
+    """
 
 def find_branch_bound_solutions_to_dataset(dataset_path, results_save_path):
     """
@@ -93,8 +99,7 @@ def generate_solution_dataset_script():
     find_branch_bound_solutions_to_dataset(dataset_path, solution_save_path)
 
 
-def get_problem_size_and_runtime_from_solution_dataset():
-    solution_save_path = './datasets/dataset_solutions_problem_size_runtime_1.pkl'
+def get_problem_size_and_runtime_from_solution_dataset(solution_save_path='./datasets/dataset_solutions_problem_size_runtime_1.pkl'):
     with open(solution_save_path, 'rb') as f:
         dataset = pickle.load(f)
 
@@ -123,17 +128,66 @@ def get_problem_size_and_runtime_from_solution_dataset():
                     problem_sizes_to_runtime_dict[problem_size] = [runtime]
     return problem_sizes_to_runtime_dict
 
-def generate_problem_size_vs_runtime_curve(problem_size_and_runtime_dict):
-    pass
+def generate_problem_size_vs_runtime_curve(problem_size_and_runtime_dict, save_path):
+    problem_sizes = []
+    runtimes = []
 
-def generate_curves(xss, yss):
-    """
-    Generate multiple curves on the same plot
-    :param xss: a list of xs
-    :param yss: a list of corresponding ys
-    :return:
-    """
+    for problem_size in problem_size_and_runtime_dict:
+        problem_sizes.append(problem_size)
+        runtimes_individual = problem_size_and_runtime_dict[problem_size]
+        runtimes.append(sum(runtimes_individual) / len(runtimes_individual)) # append the average value
+
+    # lambda for generating scatter plot (since just plotting gives horrendous curve due to much noise)
+    scatter_lambda = lambda xs, ys, ax: ax.scatter(xs,ys,marker='o')
+    generateNCurves('Problem Size','Runtime (ms)',save_path,[problem_sizes],[runtimes],0,plotter_func=scatter_lambda)
+
+
+
+# generate N different curves on the same plot
+# reused from previous project
+def generateNCurves(xlabel, ylabel, save_path, xss,yss, fig_idx,
+                    inv=False, legends=None,
+                    plotter_func=lambda xs,ys,ax: ax.plot(xs,ys,marker='o')):
+
+    plt.figure(fig_idx)
+    _, ax = plt.subplots(1, 1)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid()
+    plt.tick_params(
+        bottom=False,  # ticks along the bottom edge are off
+        top=False,  # ticks along the top edge are off,
+        left=False,
+        right=False)
+
+    # plot the curves on the same axes object
+    for xs,ys in zip(xss,yss): # get data of each curve
+        plotter_func(xs,ys,ax)
+
+    if legends is not None:
+        ax.legend(legends)  # provide legends as an array
+
+    if inv:
+        ax.invert_xaxis() # invert the x axis so that it is in descending order
+
+    plt.savefig(fname=save_path, dpi=600, bbox_inches='tight')
+
+def end_to_end_analysis():
+    dataset_save_path = './datasets/dataset_problem_size_runtime_2.pkl'
+    solution_dataset_save_path = './datasets/dataset_solutions_problem_size_runtime_2.pkl'
+    max_num_tasks = 50
+    max_num_processors = 10
+    num_tasksystems_per_iteration = 100
+    curve_save_path = './graphs/problem_size_avg_runtime.png'
+
+    # generate the tasksystems dataset
+    generate_dataset(dataset_save_path,num_tasksystems_per_iteration,max_num_tasks,max_num_processors)
+    # generate dataset with solutions and runtimes
+    find_branch_bound_solutions_to_dataset(dataset_save_path,solution_dataset_save_path)
+
+    problem_size_runtime = get_problem_size_and_runtime_from_solution_dataset(solution_dataset_save_path)
+    generate_problem_size_vs_runtime_curve(problem_size_runtime,curve_save_path)
+
 
 if __name__ == '__main__':
-    #get_problem_size_and_runtime_from_solution_dataset()
-    generate_solution_dataset_script()
+    end_to_end_analysis()

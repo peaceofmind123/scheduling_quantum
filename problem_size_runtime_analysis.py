6d2e7ed48782c2a875b1a58874c2f9c18d092184
ashishthesatan <ashishthesatan@gmail.com>
2022-07-16 11:25:12 +0545

    done with testing of refactored code

--- problem_size_runtime_analysis.py
+++ problem_size_runtime_analysis.py
@@ -90,44 +90,7 @@ def find_branch_bound_solutions_to_dataset(dataset_path, results_save_path):
     The strategy is to augment the dataset object with solution info and runtime info
     And save it in the same heirarchical (json-like) format
     """
-
-    with open(dataset_path, 'rb') as f:
-        dataset = pickle.load(f)
-
-    # count the number of infeasible problems in the dataset
-    num_infeasible_tasksystems = 0
-    total_num_tasksystems = 0
-    # iterate through the dataset
-    for num_tasks in dataset:  # num_tasks is the first key
-        for num_processors in dataset[num_tasks]:
-            task_systems_set = dataset[num_tasks][num_processors]
-
-            # this notation is needed for overwriting the values of the list
-            for i in range(len(task_systems_set)):
-                # record total number of task systems
-                total_num_tasksystems += 1
-                # initialize and solve the task system
-                task_system = task_systems_set[i]
-                bbs = BranchBoundSolver(task_system)
-
-                try:
-                    partitioning, optimal_objective, runtime_ms = bbs.solve(False)  # don't display output
-                except TypeError as e:
-                    # this happens only when the system does not have a feasible solution
-                    partitioning = 'infeasible'
-                    runtime_ms = 'infeasible'
-                    num_infeasible_tasksystems += 1
-                # now, replace the task system with the dict containing relevant info
-                task_system_info = {
-                    'task_system': task_system,
-                    'partitioning': partitioning,
-                    'runtime_ms': runtime_ms
-                }
-                task_systems_set[i] = task_system_info
-    print(f'Total number of task systems: {total_num_tasksystems}')
-    print(f'Number of infeasible task systems: {num_infeasible_tasksystems}')
-    with open(results_save_path, 'wb') as f:
-        pickle.dump(dataset, f)
+    find_solution_to_dataset(dataset_path,results_save_path,BranchBoundSolver)
 
 
 def generate_dataset_script():
@@ -232,6 +195,8 @@ def end_to_end_analysis():
 
     problem_size_runtime = get_problem_size_and_runtime_from_solution_dataset(solution_dataset_save_path)
     generate_problem_size_vs_runtime_curve(problem_size_runtime,curve_save_path)
+
+
 
 
 if __name__ == '__main__':
